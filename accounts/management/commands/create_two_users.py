aa5f101b3bd57087d5c51e931c7344195508e2dd
Sergey Malashenko <s.malash.dev@gmail.com>
2021-04-06 12:55:54 +0300

    added command for manage.py

--- accounts/management/commands/create_two_users.py
+++ accounts/management/commands/create_two_users.py
@@ -7,6 +7,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         admin = Account.objects.create_superuser('admin', 'admin@admin.com', 'admin', is_active=True)
+        user = Account.objects.create_user('test', 'test@example.com', 'test', is_active=True)
+
         if admin:
             print("Администратор создан успешно - login/password: admin/admin")
 
+        if user:
+            print("Пользователь создан успешно - login/password: test/test")
